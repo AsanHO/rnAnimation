4d6a806cce0b5f0526e4a7638b45d8e1cc618409
AsanHO <gorma00@naver.com>
2022-07-24 16:25:32 +0900

    D&D

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Animated, Pressable, TouchableOpacity } from "react-native";
+import { Animated, Pressable, PanResponder, Dimensions } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -15,40 +15,47 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  const [up, setUp] = useState(false);
-  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current; //useRef를 사용하지 않으면 마지막에 초기값으로 돌아감
-  const toggleUp = () => setUp((prev) => !prev);
-  const moveUp = () => {
-    //2.함수를 통해 Animated Method들을 통해 변수 핸들링
-    Animated.timing(POSITION, {
-      toValue: up ? 300 : -300, //변화시킬 값
-      useNativeDriver: true, //사용해야만 함 bridge를 사용하지 않음
-      duration: 2000,
-    }).start(toggleUp);
-  };
-  const opacity = POSITION.y.interpolate({
-    inputRange: [-300, 0, 300],
-    outputRange: [1, 0, 1],
-  });
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: 0,
+      y: 0,
+    })
+  ).current;
+
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
   });
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["-360deg", "360deg"],
-  });
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        POSITION.setOffset({
+          x: POSITION.x._value,
+          y: POSITION.y._value,
+        });
+      },
+      onPanResponderMove: (_, { dx, dy }) => {
+        POSITION.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+      onPanResponderRelease: () => {
+        POSITION.flattenOffset();
+      },
+    })
+  ).current;
   return (
     <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            opacity,
-            borderRadius,
-            transform: [{ rotateY: rotation }, { translateY: POSITION.y }], //변화를 주고 싶은 속성 변수 선언
-          }}
-        />
-      </Pressable>
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
+          borderRadius,
+          transform: POSITION.getTranslateTransform(),
+        }}
+      />
     </Container>
   );
 }
